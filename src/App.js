dc49ef2ec2e815b5767017c1c62d3226c5d9ebad
Ivan Pinheiro <Ivanppf1@gmail.com>
2024-03-20 09:37:14 -0300

    Primeiro commit
    crianção do projeto, remoção do código preexistente e inicio da criação da tela de cadastramento das entidades.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+    render() {
+        return (
+            //     <div className="geral">
+            //         <header className="cabecalho">
+            //             <h1>header</h1>
+            //         </header>
+            //         <main className="conteudo">
+            //             <div className="div-campos">
+            //                 <input type="checkbox"/>
+            //                 <label>is available</label>
+            //                 <br/>
+            //                 <input type="checkbox"/>
+            //                 <label>is countryside</label>
+            //                 <br/>
+            //                 <input type="checkbox"/>
+            //                 <label>has swimmingPool</label>
+            //                 <br/>
+            //                 <label>type</label>
+            //                 <br/>
+            //                 <select>
+            //                     <option value="APARTMENT">Apartment</option>
+            //                     <option value="HOUSE">House</option>
+            //                     <option value="BEDROOM">Bedroom</option>
+            //                     <option value="CABIN">Cabin</option>
+            //                     <option value="BED_N_BREAKFASTS">Bed & Breakfasts</option>
+            //                     <option value="FARM">Farm</option>
+            //                 </select>
+            //                 <br/>
+            //                 <label>description</label>
+            //                 <br/>
+            //                 <input type="text"/>
+            //             </div>
+            //         </main>
+            //         <footer className="rodape">
+            //             <h1>footer</h1>
+            //         </footer>
+            //     </div>
+
+            <div className="geral">
+                <header className="cabecalho">
+                    <h1>header</h1>
+                </header>
+                <main className="conteudo">
+                    <div>
+                        <h1 className="titulo">Room</h1>
+                    </div>
+                    <div className="div-campos">
+                        <label>width</label>
+                        <br/>
+                        <input type="text"/>
+                        <br/>
+                        <label>length</label>
+                        <br/>
+                        <input type="text"/>
+                        <br/>
+                        <label>property id</label>
+                        <br/>
+                        <input type="text"/>
+                    </div>
+                </main>
+                <footer className="rodape">
+                    <h1>footer</h1>
+                </footer>
+            </div>
+        );
+    }
 }
 
-export default App;

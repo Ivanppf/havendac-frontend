0c0ad899a7b34396f46c1363b1a440ef8a34c8b1
Ivan Pinheiro <Ivanppf1@gmail.com>
2024-04-05 10:03:55 -0300

    FIX delete comments

--- src/service/RequestService.js
+++ src/service/RequestService.js
@@ -15,23 +15,19 @@ export async function findAll(apiUrl) {
 
 export async function post(apiUrl, item) {
     await axios.post(apiUrl, item)
-        .then(response => notifySuccess("Created successfully"))
+        .then(() => notifySuccess("Created successfully"))
         .catch((error) => {
-            console.log(error)
             notifyError(`${error}`)
         })
 }
 
 export async function remove(apiUrl, id) {
-    // window.alert(`${apiUrl}/${id}`)
     await axios.delete(`${apiUrl}/${id}`)
         .then(() => notifySuccess("Deleted successfully"))
         .catch((error) => notifyError(`${error}`))
 }
 
 export async function update(apiUrl, id, item) {
-    // window.alert(`${apiUrl}/${id}`)
-    // console.log(item)
     await axios.put(`${apiUrl}/${id}`, item)
         .then(() => notifySuccess("Updated successfully"))
         .catch((error) => notifyError(`${error}`))
